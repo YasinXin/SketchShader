23398d722b064f559d945f4a23d8473a54f4b11e
yanghua Xin <xinyh@outlook.com>
2020-07-11 15:19:11 +0800

    初始化
    添加工程内容

---
+++ Assets/Scripts/MovingControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingControl : MonoBehaviour
+{
+    public float speed = 100;
+    public float speed2 = 0;
+    void Update()
+    {
+        transform.Rotate(0, Time.deltaTime * speed, 0,Space.World);
+        transform.Translate(Vector3.forward * speed2* Time.time);
+    }
+}
